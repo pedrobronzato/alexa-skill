589a22bc65ddd46fbd8fb7f32a1a71ee04920abe
unknown <pedrohotz2002@hotmail.com>
2024-12-11 11:06:07 -0300

    setup

--- index.js
+++ index.js
@@ -1,18 +1,11 @@
 import express from 'express';
 import { exec } from 'child_process'
-import 'dotenv';
 
 const app = express();
 const port = 3000;
 app.use(express.json());
 
 app.post('/shutdown', (req, res) => {
-
-    const { password } = req.body;
-
-    if (!password || password !== process.env.SECRET_PASSWORD) {
-        return res.status(403).send('Acesso negado. Senha incorreta.');
-    }
 
     exec('shutdown /s /t 0', (error, stdout, stderr) => {
         if (error) {
